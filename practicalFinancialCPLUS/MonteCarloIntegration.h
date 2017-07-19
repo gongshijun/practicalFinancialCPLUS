7d7fbe08434015fb9c13b712dbe8b4cd994df923
gongshijun-PC\gongshijun <gongshijun22761@gmail.com>
2017-07-19 09:24:48 +0800

    init the project

---
+++ practicalFinancialCPLUS/MonteCarloIntegration.h
@@ -0,0 +1,20 @@
+#pragma once
+
+template <class T>
+class MathFunction;
+
+class MonteCarloIntegration {
+public:
+	MonteCarloIntegration(MathFunction<double> &f);
+	MonteCarloIntegration(const MonteCarloIntegration &p);
+	~MonteCarloIntegration();
+	MonteCarloIntegration &operator=(const MonteCarloIntegration &p);
+
+	void setNumSamples(int n);
+	double getIntegral(double a, double b);
+	double integrateRegion(double a, double b, double min, double max);
+
+private:
+	MathFunction<double> &m_f;
+	int m_numSamples;
+};
